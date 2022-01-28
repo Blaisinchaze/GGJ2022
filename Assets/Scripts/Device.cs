35d7d401578dcee5fece30e2c05badf9851bbc5a
Quinn McNeil <quinn.mcneil@tidygames.co.uk>
2022-01-28 21:47:44 +0000

    Added Devices + enemy start

---
+++ Assets/Scripts/Device.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum PowerState
+{
+    DRAINED,
+    CHARGING,
+    POWERED
+}
+
+public abstract class Device : MonoBehaviour
+{
+    [Header("Device Settings")]
+    public float maxEnergy;
+    public float minActiveEnergy;
+    public float energyLossPerSecond;
+    [Space]
+    internal float currentEnergy;
+    internal Stack<PowerState> stateStack;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentEnergy = maxEnergy;
+        stateStack.Push(PowerState.POWERED);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        StateUpdate();
+        SpendEnergy(energyLossPerSecond * Time.deltaTime);
+    }
+
+    // returns how much over the maxEnergy
+    public float AddEnergy(float input)
+    {
+        float retVal = 0;
+        if (currentEnergy + input > maxEnergy)
+        {
+            retVal = (currentEnergy + input) - maxEnergy;
+        }
+
+        currentEnergy += input;
+
+        currentEnergy = Mathf.Clamp(currentEnergy, 0, maxEnergy);
+
+
+        return retVal;
+    }
+
+    // returns how much energy is being spent
+    public float SpendEnergy(float cost)
+    {
+        float retVal = cost;
+
+        if (currentEnergy - cost < 0)
+        {
+            retVal = currentEnergy;
+        }
+
+        currentEnergy -= cost;
+        currentEnergy = Mathf.Clamp(currentEnergy, 0, maxEnergy);
+
+        return retVal;
+    }
+
+    abstract internal void StateUpdate();
+}
---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : Device
+{
+    [Header("Enemy Settings")]
+    public float chargeRatePerSecond;
+    public float minDistanceFromPlayer;
+
+    NavMeshAgent agent;
+    Transform player;
+    bool shielded;
+    Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        shielded = false;
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        StateUpdate();
+
+        switch (stateStack.Peek())
+        {
+            case PowerState.DRAINED:
+                break;
+            case PowerState.CHARGING:
+                currentEnergy += chargeRatePerSecond * Time.deltaTime;
+                break;
+            case PowerState.POWERED:
+                UpdatePathing(player.position);
+                break;
+            default:
+                break;
+        }
+    }
+
+    override internal void StateUpdate()
+    {
+        switch (stateStack.Peek())
+        {
+            case PowerState.DRAINED:
+                stateStack.Pop();
+                stateStack.Push(PowerState.CHARGING);
+                break;
+
+            case PowerState.CHARGING:
+                if (currentEnergy == maxEnergy)
+                {
+                    stateStack.Clear();
+                    stateStack.Push(PowerState.POWERED);
+                }
+                break;
+
+            case PowerState.POWERED:
+                if (currentEnergy == 0)
+                {
+                    stateStack.Push(PowerState.CHARGING);
+                }
+                break;
+
+            default:
+                break;
+        }
+        
+    }
+
+    private void UpdatePathing(Vector3 position)
+    {
+        agent.SetDestination(position);
+    }
+}
