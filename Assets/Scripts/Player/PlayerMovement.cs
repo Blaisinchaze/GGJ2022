8ad18ea7177cb864cb85d0f12f15699e30c4d4bc
Sol Morrison <solomon2.morrison@live.uwe.ac.uk>
2022-01-28 20:33:24 +0000

    Working on Player Movement

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    /// <summary>
+    /// Rigidbody attached to the player
+    /// </summary>
+    [SerializeField] private Rigidbody Rb;
+    public enum MovementState
+    {
+        Neutral = 0,
+        Moving,
+        Jumping
+    }
+
+    /// <summary>
+    /// Whether input will be read to move the player.
+    /// </summary>
+    private bool canMove;
+
+    /// <summary>
+    /// Speed player moves in any direction.
+    /// </summary>
+    public float MoveSpeed;
+    
+    
+    void Start()
+    {
+        Setup();
+    }
+
+    void Update()
+    {
+        HandleInputs();
+    }
+
+    private void Setup()
+    {
+        Rb = GetComponent<Rigidbody>();
+    }
+    private void HandleInputs()
+    {
+        //  Jumping
+        
+        //  Movement
+        if (!canMove) return;
+        if (Input.GetKeyDown("W"))
+            MoveInDirection(Vector3.forward);
+        if (Input.GetKeyDown("S"))
+            MoveInDirection(-Vector3.forward);
+        
+        if (Input.GetKeyDown("A"))
+            MoveInDirection(Vector3.left);
+        if (Input.GetKeyDown("D"))
+            MoveInDirection(-Vector3.left);
+    }
+
+    private void MoveInDirection(Vector3 dir)
+    {
+        Rb.velocity = dir * MoveSpeed ;
+    }
+}
