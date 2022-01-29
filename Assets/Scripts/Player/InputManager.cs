8c9eada4d2d3e1b8dbaaa0864f77cddad64e34f7
Sol Morrison <solomon2.morrison@live.uwe.ac.uk>
2022-01-29 01:16:31 +0000

    Really wonky stuff

---
+++ Assets/Scripts/Player/InputManager.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+using Cursor = UnityEngine.Cursor;
+
+public class InputManager : MonoBehaviour
+{
+    /// <summary>
+    /// Movement direction determined from pressing WASD
+    /// </summary>
+    /// <returns></returns>
+    private Vector2 MovementDirection = Vector2.zero;
+
+    /// <summary>
+    /// Look direction determined from moving the mouse
+    /// </summary>
+    private Vector2 LookDirection = Vector2.zero;
+
+    private bool jumpPressed = false;
+    
+    //Replace this when we have a gamestate manager
+    public bool InGame = true;
+    
+    void Start()
+    {
+        if (InGame)
+        {
+            Cursor.visible = false;
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+    }
+    private void Update()
+    {
+
+        UpdateLookDirection();
+        UpdateMovementDirection();
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+        }
+    }
+
+    #region Public Calls
+
+    public Vector2 GetWASD()
+    {
+        return MovementDirection;
+    }
+
+    public Vector2 GetMouseDir()
+    {
+        return LookDirection;
+    }
+
+    public bool GetIsJumping()
+    {
+        return jumpPressed;
+    }
+    #endregion
+    #region Local Stuff
+    private void UpdateMovementDirection()
+    {
+        MovementDirection = Vector2.zero;
+        if (Input.GetKey(KeyCode.W))
+            MovementDirection = new Vector2(MovementDirection.x, MovementDirection.y + 1);
+        if (Input.GetKey(KeyCode.S))
+            MovementDirection = new Vector2(MovementDirection.x, MovementDirection.y - 1);
+        
+        if (Input.GetKey(KeyCode.A))
+            MovementDirection = new Vector2(MovementDirection.x - 1, MovementDirection.y);
+        if (Input.GetKey(KeyCode.D))
+            MovementDirection = new Vector2(MovementDirection.x + 1, MovementDirection.y);
+    }
+
+    private void UpdateLookDirection()
+    {
+        LookDirection = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+    }
+
+    private void UpdateJump()
+    {
+        jumpPressed = false;
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            jumpPressed = true;
+        }
+    }
+
+
+    #endregion
+}
--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -8,13 +8,19 @@ public class PlayerMovement : MonoBehaviour
     /// Rigidbody attached to the player
     /// </summary>
     [SerializeField] private Rigidbody Rb;
-    public enum MovementState
-    {
-        Neutral = 0,
-        Moving,
-        Jumping
-    }
+    [SerializeField] private GameObject Body;
+    [SerializeField] private Transform cameraTransform;
+    
+    /// <summary>
+    /// Script to check for grounded state
+    /// </summary>
+    [SerializeField] private GroundDetection groundCheck;
 
+    /// <summary>
+    /// Receive inputs from this script to execute
+    /// </summary>
+    [SerializeField] private InputManager inputManager;
+    
     /// <summary>
     /// Whether input will be read to move the player.
     /// </summary>
@@ -23,9 +29,10 @@ public class PlayerMovement : MonoBehaviour
     /// <summary>
     /// Speed player moves in any direction.
     /// </summary>
-    public float MoveSpeed;
-    
-    
+    public float accelRateGround, accelRateAir;
+
+    [SerializeField] private float velocityGroundLimit, velocityAirLimit;
+
     void Start()
     {
         Setup();
@@ -33,32 +40,94 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
-        HandleInputs();
+        if (inputManager.GetIsJumping() && !groundCheck.GetIsGrounded())
+        {
+            Rb.velocity = new Vector3(Rb.velocity.x, 100, Rb.velocity.z);
+        }
     }
 
-    private void Setup()
+    void FixedUpdate()
     {
-        Rb = GetComponent<Rigidbody>();
-    }
-    private void HandleInputs()
-    {
-        //  Jumping
-        
-        //  Movement
-        if (!canMove) return;
-        if (Input.GetKeyDown("W"))
-            MoveInDirection(Vector3.forward);
-        if (Input.GetKeyDown("S"))
-            MoveInDirection(-Vector3.forward);
-        
-        if (Input.GetKeyDown("A"))
-            MoveInDirection(Vector3.left);
-        if (Input.GetKeyDown("D"))
-            MoveInDirection(-Vector3.left);
+        RotateBodyWithCamera();
+        ConvertInputsToMovementDir();
     }
 
+    private void Setup() 
+    {
+    }
+
+    /// <summary>
+    /// Convert movement inputs from Vector2 to Vector3
+    /// </summary>
+    private void ConvertInputsToMovementDir()
+    {
+        var inputs = inputManager.GetWASD();
+        if (inputs == Vector2.zero && groundCheck.GetIsGrounded())
+        {
+            Rb.velocity = Vector3.zero;
+            return;
+        }
+
+        Vector3 moveDir;
+        if (groundCheck.GetIsGrounded()) 
+            moveDir = new Vector3(inputs.x, 0, inputs.y);
+        else
+            moveDir = new Vector3(inputs.x, Rb.velocity.y, inputs.y);
+
+
+        moveDir = Body.transform.TransformDirection(moveDir);
+        
+        MoveInDirection(moveDir.normalized); 
+    }
+    /// <summary>
+    /// Accelerate player over time in direction they are aiming
+    /// </summary>
+    /// <param name="dir"></param>
     private void MoveInDirection(Vector3 dir)
     {
-        Rb.velocity = dir * MoveSpeed ;
+        Rb.velocity += dir * (accelRateGround * Time.deltaTime);
+        Rb.velocity = VelocityChecks();
+        print(dir.ToString());
+    }
+
+    private Vector3 VelocityChecks()
+    {
+        var velCheck = Rb.velocity;
+        
+        if (velCheck.x > velocityGroundLimit && groundCheck.GetIsGrounded())
+            velCheck = new Vector3(velocityGroundLimit, velCheck.y, velCheck.z);
+        else if (velCheck.x > velocityAirLimit && !groundCheck.GetIsGrounded())
+            velCheck = new Vector3(velocityAirLimit, velCheck.y, velCheck.z);
+        
+        if (velCheck.x < -velocityGroundLimit && groundCheck.GetIsGrounded())
+            velCheck = new Vector3(-velocityGroundLimit, velCheck.y, velCheck.z);
+        else if (velCheck.x < -velocityAirLimit && !groundCheck.GetIsGrounded())
+            velCheck = new Vector3(-velocityAirLimit, velCheck.y, velCheck.z);
+
+        if (velCheck.y > 5)  
+            velCheck = new Vector3(velCheck.x, 5, velCheck.z);
+        if (velCheck.y < -5)
+            velCheck = new Vector3(velCheck.x, -15, velCheck.z);
+                
+        if (velCheck.z > velocityGroundLimit && groundCheck.GetIsGrounded())
+            velCheck = new Vector3(velCheck.x, velCheck.y, velocityGroundLimit);
+        else if (velCheck.z > velocityAirLimit && !groundCheck.GetIsGrounded())
+            velCheck = new Vector3(velCheck.x, velCheck.y, velocityAirLimit);
+        
+        if (velCheck.z < -velocityGroundLimit && groundCheck.GetIsGrounded())
+            velCheck = new Vector3(velCheck.x, velCheck.y, -velocityGroundLimit);
+        else if (velCheck.z < -velocityAirLimit && !groundCheck.GetIsGrounded())
+            velCheck = new Vector3(velCheck.x, velCheck.y, -velocityAirLimit);
+
+        return velCheck;
+    }
+    /// <summary>
+    /// Rotate the player's body to follow the Y of the Camera.
+    /// </summary>
+    private void RotateBodyWithCamera()
+    {
+        var rotation = Body.transform.rotation;
+        rotation = new Quaternion(rotation.x, cameraTransform.forward.y, rotation.z, rotation.w);
+        Body.transform.rotation = rotation;
     }
 }
