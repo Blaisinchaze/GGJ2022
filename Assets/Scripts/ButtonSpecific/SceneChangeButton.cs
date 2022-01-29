cf584e91a76b11d8a4f5ac4ee39892eeefbfd340
Quinn McNeil <quinn.mcneil@tidygames.co.uk>
2022-01-29 19:08:32 +0000

    Added Main Menu

---
+++ Assets/Scripts/ButtonSpecific/SceneChangeButton.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SceneChangeButton : MonoBehaviour
+{
+    public string sceneName;
+    public GameState state;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(LateStart());
+    }
+
+    IEnumerator LateStart()
+    {
+        yield return new WaitForSeconds(0.5f);
+        GetComponent<Button>().onClick.AddListener(() => GameManager.Instance.ChangeScene(sceneName));
+        GetComponent<Button>().onClick.AddListener(() => GameManager.Instance.ChangeState(state));
+    }
+}
