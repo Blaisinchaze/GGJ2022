5af81916a96b9632d4025226c475f2a7a3386e74
Sol Morrison <solomon2.morrison@live.uwe.ac.uk>
2022-01-30 16:31:17 +0000

    Energy drain over time changed

--- Assets/Scripts/Devices/Device.cs
+++ Assets/Scripts/Devices/Device.cs
@@ -90,15 +90,15 @@ public class Device : MonoBehaviour
         powerState = currentEnergy >= minActiveEnergy ? PowerState.POWERED : PowerState.DRAINED;
     }
 
-    public IEnumerator StealEnergyOverTime(Device victim, float TimeFromMax)
+    public IEnumerator StealEnergyOverTime(Device victim, float speed)
     {
-        StealEnergy(victim, victim.maxEnergy / TimeFromMax * Time.deltaTime);
+        StealEnergy(victim, speed * Time.deltaTime);
         yield return 0;
     }
     
-    public IEnumerator SendEnergyOverTime(Device receiver, float TimeFromMax)
+    public IEnumerator SendEnergyOverTime(Device receiver, float speed)
     {
-        receiver.StealEnergy(this, maxEnergy / TimeFromMax * Time.deltaTime);
+        receiver.StealEnergy(this, speed * Time.deltaTime);
         yield return 0;
     }
 }
