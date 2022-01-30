fdaa1202a767fcc088ec338368a9c4d58d50f869
Quinn McNeil <quinn.mcneil@tidygames.co.uk>
2022-01-30 12:05:57 +0000

    Upgrades and Vending machine

---
+++ Assets/Scripts/Devices/Upgrade.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Upgrade : MonoBehaviour
+{
+    public int healthUpgrade;
+    public float speedUpgrade;
+    public float drainUpgrade;
+    public float batteryUpgrade;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Combatant cb = other.GetComponent<Combatant>();
+            cb.health += healthUpgrade;
+            cb.maxHealth += healthUpgrade;
+            cb.maxEnergy += batteryUpgrade;
+            PlayerWiring wr = other.GetComponent<PlayerWiring>();
+            wr.drainTime += drainUpgrade;
+            PlayerMovement mv = other.GetComponent<PlayerMovement>();
+            mv.moveSpeed += speedUpgrade;
+        }
+    }
+}
